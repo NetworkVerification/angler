46177822377ffb0d0466a4badf43fe6869578293
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2022-03-04 16:11:38 -0500

    More convert_expr cases

--- convert.py
+++ convert.py
@@ -3,10 +3,15 @@
 Conversion tools to transform Batfish AST terms to Angler AST terms.
 """
 import bast.expression as bexpr
-import bast.boolexprs as bbools
+import bast.boolexprs as bools
 import bast.communities as bcomms
-import bast.longexprs as blongs
+import bast.longexprs as longs
+import bast.prefix as prefix
+from bast.btypes import Comparator
 import aast.expression as aexpr
+
+# the argument to the transfer
+ARG = aexpr.Var("route")
 
 
 def convert_expr(b: bexpr.Expression) -> aexpr.Expression:
@@ -16,28 +21,28 @@ def convert_expr(b: bexpr.Expression) -> aexpr.Expression:
     match b:
         case bexpr.CallExpr(policy):
             return aexpr.CallExpr(policy)
-        case bbools.StaticBooleanExpr(ty=bbools.StaticBooleanExprType.TRUE):
+        case bools.StaticBooleanExpr(ty=bools.StaticBooleanExprType.TRUE):
             return aexpr.LiteralTrue()
-        case bbools.StaticBooleanExpr(ty=bbools.StaticBooleanExprType.FALSE):
+        case bools.StaticBooleanExpr(ty=bools.StaticBooleanExprType.FALSE):
             return aexpr.LiteralFalse()
-        case bbools.StaticBooleanExpr(ty=bbools.StaticBooleanExprType.CALLCONTEXT):
+        case bools.StaticBooleanExpr(ty=bools.StaticBooleanExprType.CALLCONTEXT):
             # NOTE: not supported
             return aexpr.Havoc()
-        case bbools.Conjunction(conjuncts):
+        case bools.Conjunction(conjuncts):
             conj = [convert_expr(c) for c in conjuncts]
             return aexpr.Conjunction(conj)
-        case bbools.Disjunction(disjuncts):
+        case bools.Disjunction(disjuncts):
             disj = [convert_expr(d) for d in disjuncts]
             return aexpr.Disjunction(disj)
-        case bbools.Not(e):
+        case bools.Not(e):
             return aexpr.Not(convert_expr(e))
-        case bbools.MatchIpv4():
+        case bools.MatchIpv4():
             # NOTE: for now, we assume ipv4
             return aexpr.LiteralTrue()
-        case bbools.MatchIpv6 | bbools.MatchPrefix6Set:
+        case bools.MatchIpv6 | bools.MatchPrefix6Set:
             # NOTE: not supported (for now, we assume ipv4)
             return aexpr.LiteralFalse()
-        case bbools.LegacyMatchAsPath:
+        case bools.LegacyMatchAsPath:
             # NOTE: not supported
             return aexpr.Havoc()
         case bcomms.LiteralCommunitySet(comms):
@@ -52,25 +57,41 @@ def convert_expr(b: bexpr.Expression) -> aexpr.Expression:
         case bcomms.CommunitySetDifference(initial, bcomms.CommunityIs(community)):
             # remove a single community from the set
             return aexpr.SetRemove(community, convert_expr(initial))
-        case bbools.MatchCommunities(
+        case bools.MatchCommunities(
             _comms, bcomms.HasCommunity(bcomms.CommunityIs(community))
         ):
             # check if community is in _comms
             return aexpr.SetContains(community, convert_expr(_comms))
         case bcomms.InputCommunities:
-            arg = aexpr.Var("route")
-            return aexpr.GetField(arg, "communities")
+            return aexpr.GetField(ARG, "communities")
         case bcomms.CommunitySetReference(_name) | bcomms.CommunityMatchExprReference(
             _name
         ) | bcomms.CommunitySetMatchExprReference(_name):
             return aexpr.Var(_name)
-        case blongs.LiteralLong(value):
+        case longs.LiteralLong(value):
             return aexpr.LiteralInt(value)
-        case blongs.IncrementLocalPref(addend):
-            arg = aexpr.Var("route")
-            return aexpr.Add(aexpr.GetField(arg, "lp"), aexpr.LiteralInt(addend))
-        case blongs.DecrementLocalPref(subtrahend):
-            arg = aexpr.Var("route")
-            return aexpr.Sub(aexpr.GetField(arg, "lp"), aexpr.LiteralInt(subtrahend))
+        case longs.IncrementLocalPref(addend):
+            return aexpr.Add(aexpr.GetField(ARG, "lp"), aexpr.LiteralInt(addend))
+        case longs.DecrementLocalPref(subtrahend):
+            return aexpr.Sub(aexpr.GetField(ARG, "lp"), aexpr.LiteralInt(subtrahend))
+        case prefix.DestinationNetwork:
+            return aexpr.GetField(ARG, "prefix")
+        case prefix.NamedPrefixSet(_name):
+            return aexpr.Var(_name)
+        case bools.MatchPrefixSet(_prefix, _prefixes):
+            return aexpr.PrefixContains(convert_expr(_prefix), convert_expr(_prefixes))
+        case bools.MatchTag(cmp, tag):
+            route_tag = aexpr.GetField(ARG, "tag")
+            match cmp:
+                case Comparator.EQ:
+                    return aexpr.Equal(route_tag, convert_expr(tag))
+                case Comparator.LE:
+                    return aexpr.LessThanEqual(route_tag, convert_expr(tag))
+                case Comparator.LT:
+                    return aexpr.LessThan(route_tag, convert_expr(tag))
+                case Comparator.GE:
+                    return aexpr.GreaterThanEqual(route_tag, convert_expr(tag))
+                case Comparator.GT:
+                    return aexpr.GreaterThan(route_tag, convert_expr(tag))
         case _:
             raise NotImplementedError(f"No convert case for {b} found.")
