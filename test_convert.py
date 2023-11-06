fbc3d346a25dc0c1c5083c4a06a5ed1f57d18b3b
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2023-11-06 13:01:32 -0500

    Add more convert_routing_policy tests

--- test_convert.py
+++ test_convert.py
@@ -23,7 +23,56 @@ def test_convert_return_true_statement():
     ]
 
 
-def test_convert_routing_policy():
+def test_convert_exit_reject_statement():
+    old = bsm.StaticStatement(bsm.StaticStatementType.EXIT_REJECT)
+    new = convert_stmt(old)
+    assert new == [
+        update_arg(create_result(_value=False, _exit=True), aty.EnvironmentType.RESULT)
+    ]
+
+
+def test_convert_routing_policy_empty():
+    old = []
+    new = convert_routing_policy(old)
+    assert new.body == [
+        asm.IfStatement(
+            "reset_return",
+            unreachable(),
+            [update_arg_result(_return=False)],
+            [
+                update_arg_result(
+                    _fallthrough=True,
+                    _value=get_arg(aty.EnvironmentType.LOCAL_DEFAULT_ACTION),
+                )
+            ],
+        )
+    ]
+
+
+def test_convert_routing_policy_single():
+    old: list[bsm.Statement] = [
+        bsm.StaticStatement(bsm.StaticStatementType.RETURN_TRUE),
+    ]
+    new = convert_routing_policy(old)
+    assert new.body == [
+        update_arg(
+            create_result(_value=True, _return=True), aty.EnvironmentType.RESULT
+        ),
+        asm.IfStatement(
+            "reset_return",
+            unreachable(),
+            [update_arg_result(_return=False)],
+            [
+                update_arg_result(
+                    _fallthrough=True,
+                    _value=get_arg(aty.EnvironmentType.LOCAL_DEFAULT_ACTION),
+                )
+            ],
+        ),
+    ]
+
+
+def test_convert_routing_policy_return_true_two():
     old = [
         bsm.StaticStatement(bsm.StaticStatementType.RETURN_TRUE),
         bsm.SetLocalPreference(ble.LiteralLong(200)),
@@ -38,6 +87,46 @@ def test_convert_routing_policy():
             unreachable(),
             [],
             [update_arg(aex.LiteralUInt(200), aty.EnvironmentType.LP)],
+        ),
+        asm.IfStatement(
+            "reset_return",
+            unreachable(),
+            [update_arg_result(_return=False)],
+            [
+                update_arg_result(
+                    _fallthrough=True,
+                    _value=get_arg(aty.EnvironmentType.LOCAL_DEFAULT_ACTION),
+                )
+            ],
+        ),
+    ]
+
+
+def test_convert_routing_policy_three():
+    old = [
+        bsm.SetLocalPreference(ble.LiteralLong(200)),
+        bsm.StaticStatement(bsm.StaticStatementType.EXIT_REJECT),
+        bsm.SetLocalPreference(ble.LiteralLong(100)),
+    ]
+    new = convert_routing_policy(old)
+    assert new.body == [
+        update_arg(aex.LiteralUInt(200), aty.EnvironmentType.LP),
+        asm.IfStatement(
+            "early_return",
+            unreachable(),
+            [],
+            [
+                # EXIT_REJECT statement
+                update_arg(
+                    create_result(_value=False, _exit=True), aty.EnvironmentType.RESULT
+                ),
+                asm.IfStatement(
+                    "early_return",
+                    unreachable(),
+                    [],
+                    [update_arg(aex.LiteralUInt(100), aty.EnvironmentType.LP)],
+                ),
+            ],
         ),
         asm.IfStatement(
             "reset_return",
